ed53ff782497cfcecd28808a9a5df8db6cee29e0
Mehmet Nuri Akyol <eksglox@yandex.com>
2022-07-03 16:08:34 +0300

    Initial Commit

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by mehmet  akyol on 3.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
